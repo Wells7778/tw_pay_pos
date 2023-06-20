1a66296e30238c23432edeb3ee5478731dd5b391
Wells <chawkeye@gmail.com>
2023-06-20 13:56:54 +0800

    fix: query status

--- lib/tw_pay_pos/response/query.rb
+++ lib/tw_pay_pos/response/query.rb
@@ -3,6 +3,18 @@ require_relative 'pay'
 module TwPayPos
   module Response
     class Query < Pay
+      def success?
+        super && order_success?
+      end
+
+      def order_success?
+        %w[03 05].include?(order_status)
+      end
+
+      def order_status
+        @status
+      end
+
       def refund_amount
         @refund_amount / 100
       end
